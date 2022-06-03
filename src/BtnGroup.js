5ee07eb0564252a3082be7cf6da904a4447229d6
aselwilliams <aselwilliams22@gmail.com>
2022-06-03 10:58:04 -0400

    formatted the code

--- src/BtnGroup.js
+++ src/BtnGroup.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React from "react";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
-function BtnGroup({index, setIndex, slides}) {
+function BtnGroup({ index, setIndex, slides }) {
   return (
     <div>
-    {index > 0 ? (
-      <FaArrowAltCircleLeft
-        onClick={() => setIndex(index - 1)}
-        size="40px" color='rgb(31, 132, 165)'
-      />
-    ) : null}
-    {index < slides.length - 1 ? (
-      <FaArrowAltCircleRight
-        onClick={() => setIndex(index + 1)}
-        size="40px" color='rgb(31, 132, 165)'
-      />
-    ) : null}
-  </div>
-  )
+      {index > 0 ? (
+        <FaArrowAltCircleLeft
+          onClick={() => setIndex(index - 1)}
+          size="40px"
+          color="rgb(31, 132, 165)"
+        />
+      ) : null}
+      {index < slides.length - 1 ? (
+        <FaArrowAltCircleRight
+          onClick={() => setIndex(index + 1)}
+          size="40px"
+          color="rgb(31, 132, 165)"
+        />
+      ) : null}
+    </div>
+  );
 }
 
-export default BtnGroup
+export default BtnGroup;
--- src/Slides.js
+++ src/Slides.js
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 
-function Slides({slides, index}) {
+function Slides({ slides, index }) {
   return (
     <main className="wrapper">
-    {slides.map((slide, i) => {
-      let position = "nextSlide";
-      if (i === index) position = "activeSlide";
-      if (i === index - 1) position = "lastSlide";
-      return (
-        <section key={slide.id} className={position}>
-          <img src={slide.image} alt="nature" />
-          <span className="page">{slide.id}</span>
-        </section>
-      );
-    })}
-  </main>
-  )
+      {slides.map((slide, i) => {
+        let position = "nextSlide";
+        if (i === index) position = "activeSlide";
+        if (i === index - 1) position = "lastSlide";
+        return (
+          <section key={slide.id} className={position}>
+            <img src={slide.image} alt="nature" />
+            <span className="page">{slide.id}</span>
+          </section>
+        );
+      })}
+    </main>
+  );
 }
 
-export default Slides
+export default Slides;
