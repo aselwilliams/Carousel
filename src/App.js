81b34b9693b75c7bea349717580959ed3634c5e7
aselwilliams <aselwilliams22@gmail.com>
2022-06-03 10:27:12 -0400

    data imported, ui painted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import { useState } from "react";
+import "./App.css";
+import data from "./data";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
-import './App.css';
-
-function App() {
+export default function App() {
+  const [slides, setSlides] = useState(data);
+  const [index, setIndex] = useState(1);
   return (
     <div className="App">
-
+      <main className="wrapper">
+        {slides.map((slide, i) => {
+          let position = "nextSlide";
+          if (i === index) position = "activeSlide";
+          if (i === index - 1) position = "lastSlide";
+          return (
+            <section key={slide.id} className={position}>
+              <img src={slide.image} alt="nature" />
+              <span className="page">{slide.id}</span>
+            </section>
+          );
+        })}
+      </main>
+      <div>
+        {index > 0 ? (
+          <FaArrowAltCircleLeft
+            onClick={() => setIndex(index - 1)}
+            size="30px"
+          />
+        ) : null}
+        {index < slides.length - 1 ? (
+          <FaArrowAltCircleRight
+            onClick={() => setIndex(index + 1)}
+            size="30px"
+          />
+        ) : null}
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,39 @@
+const slides = [
+    {
+      id: 1,
+      image:
+        "https://images.unsplash.com/photo-1532274402911-5a369e4c4bb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODR8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+      id: 2,
+      image:
+        "https://images.unsplash.com/photo-1503614472-8c93d56e92ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTF8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+      id: 3,
+      image:
+        "https://images.unsplash.com/photo-1533371452382-d45a9da51ad9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODl8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+      id: 4,
+      image:
+        "https://images.unsplash.com/photo-1438786657495-640937046d18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODJ8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+      id: 5,
+      image:
+        "https://images.unsplash.com/photo-1542856391-010fb87dcfed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTMyfHxuYXR1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+      id: 6,
+      image:
+        "https://images.unsplash.com/photo-1468413253725-0d5181091126?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTg5fHxuYXR1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+      id: 7,
+      image:
+        "https://images.unsplash.com/photo-1518593929011-2b5ef6be57c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTg2fHxuYXR1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+    }
+  ];
+  export default slides;
+  
